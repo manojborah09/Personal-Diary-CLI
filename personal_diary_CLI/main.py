e86b78e745431de56b1aacc419b8d4c6781a7b1d
manojborah09 <mb5620601@gmail.com>
2026-05-14 14:31:18 +0530

    Delete and Search funtions

--- personal_diary_CLI/main.py
+++ personal_diary_CLI/main.py
@@ -30,17 +30,30 @@ def load_entries():
 
   return content_of_json
 
+def delete_entry():
+  view_entry()
+
+  num = int(input("Enter entry number to delete :"))
+  diary.pop(num-1)
+
+
+def search_entry():
+  keyword = input("Enter the keyword the search the entry : ")
+
+  for entry in diary:
+    if keyword.lower() in entry['Title'] or keyword.lower() in entry['Content']:
+      
+      print("===== MATCH FOUND =====")
+
+      for key , value in entry.items():
+        print(key,value)
+
 def main():
 
-  print('''====================
-  PERSONAL DIARY CLI
-    ====================''')
+  print('''==================== \n PERSONAL DIARY CLI \n ==================== \n''')
     
-  print('''1. Add Entry
-  2. View Entries
-  3. Exit''')
+  print('''1. Add Entry \n 2. View Entries \n 3. Delete \n 4. Search \n 5. Exit \n''')
     
-
   while True:
 
     option = int(input("Choose an option : "))
@@ -52,6 +65,12 @@ def main():
       view_entry()
 
     elif option == 3:
+      delete_entry()
+
+    elif option == 4:
+      search_entry()
+
+    elif option == 5:
       exit_program()
       break
 
